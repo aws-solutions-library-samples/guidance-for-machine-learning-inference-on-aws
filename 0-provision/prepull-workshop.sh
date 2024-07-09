0dc973780361516ab8f13b8fc997fa930f6a8db8
Alex Iankoulski <iankouls@amazon.com>
2024-07-09 17:05:53 -0400

    Add status and stop command to prepull-workshop.sh, add graviton prepull

--- 0-provision/prepull-workshop.sh
+++ 0-provision/prepull-workshop.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
-./prepull.sh start model inf
-./prepull.sh start test cpu
+help () {
+	echo ""
+	echo "Usage: $0 [ACTION]"
+	echo "       ACTION - start(default), status, stop, help"
+	echo ""
+}
 
+export ACTION=$1
+if [ "$ACTION" == "help" ]; then
+	help
+elif [ "$ACTION" == "start" ]; then
+	./prepull.sh start model inf
+	./prepull.sh start model graviton
+	./prepull.sh start test cpu
+elif [ "$ACTION" == "status" ]; then
+	CMD="kubectl get ds -A | grep -E 'READY|prepull'"
+	echo ""
+	echo "$CMD"
+	eval "$CMD"
+elif [ "$ACTION" == "stop" ]; then
+	./prepull.sh stop model inf
+	./prepull.sh stop model graviton
+	./prepull.sh stop test cpu
+else
+	echo ""
+	echo "Invalid action: $ACTION"
+	help
+fi
+	
